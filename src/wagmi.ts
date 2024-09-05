b9a2023f44d8f17795d8aaecfeafa04b39531381
dariovp <dariovp01@gmail.com>
2024-09-05 15:38:16 -0300

    Add zetachain as mainchain

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,13 +1,13 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
-	scroll
+	zetachain
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
 	appName: 'RainbowKit demo',
 	projectId: 'YOUR_PROJECT_ID',
 	chains: [
-		scroll
+		zetachain
 	],
 	ssr: true,
 });
